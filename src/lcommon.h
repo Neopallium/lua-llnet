e6cc0e38fd56d0f58a30f37b2ff0411bf5a00549
Robert G. Jakabosky <bobby@sharedrealm.com>
2012-02-26 20:02:40 -0800

    Copy new lcommon.h from lua-buf.

--- src/lcommon.h
+++ src/lcommon.h
@@ -1,12 +1,9 @@
 /***************************************************************************
- *   Copyright (C) 2011 by Robert G. Jakabosky <bobby@sharedrealm.com>     *
+ * Copyright (C) 2007-2012 by Robert G. Jakabosky <bobby@neoawareness.com> *
  *                                                                         *
  ***************************************************************************/
 #if !defined(__L_COMMON_H__)
 #define __L_COMMON_H__
-
-#define L_LIB_API extern
-#define L_INLINE static inline
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
@@ -14,10 +11,64 @@
 
 #include <assert.h>
 #include <stdio.h>
+
+#ifdef _MSC_VER
+#define __WINDOWS__
+#else
+#if defined(_WIN32)
+#define __WINDOWS__
+#endif
+#endif
+
+#ifdef __WINDOWS__
+
+/* for MinGW32 compiler need to include <stdint.h> */
+#ifdef __GNUC__
 #include <stdint.h>
 #include <stdbool.h>
-#include <sys/types.h>
-#include <sys/user.h>
+#else
+
+/* define some standard types missing on Windows. */
+#ifndef __INT8_MAX__
+typedef __int8 int8_t;
+typedef unsigned __int8 uint8_t;
+#endif
+#ifndef __INT16_MAX__
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+#endif
+#ifndef __INT32_MAX__
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+#endif
+#ifndef __INT64_MAX__
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#endif
+typedef int bool;
+#ifndef true
+#define true 1
+#endif
+#ifndef false
+#define false 0
+#endif
+
+#endif
+
+#define L_LIB_API __declspec(dllexport)
+#define L_INLINE static
+
+#else
+
+#define L_LIB_API extern
+#define L_INLINE static inline
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define L_FUNC_UNUSED __attribute__((unused))
+
+#endif
 
 #ifdef NDEBUG
 #define l_assert(expr) do { } while(0)
@@ -44,7 +95,6 @@
 
 #define L_GNUC_PACKED __attribute__((__packed__))
 #define L_UNUSED(var) ((void)var)
-#define L_FUNC_UNUSED __attribute__((unused))
 
 /* L_LIKELY/L_UNLIKELY */
 #if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
